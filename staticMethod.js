6d8f847729cb81d07801956fe5202891e676454d
SM-junior <shahinalam12341@gmail.com>
2023-10-03 01:23:45 +0600

    class inheritance is done

--- staticMethod.js
+++ staticMethod.js
@@ -1,19 +1,19 @@
-class Rectangle{
-    constructor (name, height, width){
-        this.name=name;
-        this.height=height;
-        this.width=width
-    }
-    area(){
-        return this.height*this.width
-    }
-    //static method
-    static getClass(){
-        return 'Shahin is not a bad boy'
-    }
-}
-const rect= new Rectangle('Shahin',20,20)
-console.log(rect.area());
+// class Rectangle{
+//     constructor (name, height, width){
+//         this.name=name;
+//         this.height=height;
+//         this.width=width
+//     }
+//     area(){
+//         return this.height*this.width
+//     }
+//     //static method
+//     static getClass(){
+//         return 'Shahin is not a bad boy'
+//     }
+// }
+// const rect= new Rectangle('Shahin',20,20)
+// console.log(rect.area());
 
-// access by static method
-console.log(Rectangle.getClass());
+// // access by static method
+// console.log(Rectangle.getClass());
